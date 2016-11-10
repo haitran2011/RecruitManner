f4cfa25fedc5353e92d9bcd43f792fa4544724c2
jigang.duan <jigang.duan@tcl.com>
2016-11-10 13:07:10 +0800

    new project

---
+++ RecruitManner/AlamofireObjectMapper/AlamofireObjectMapper.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireObjectMapper.h
+//  AlamofireObjectMapper
+//
+//  Created by Jiang Duan on 16/11/10.
+//  Copyright © 2016年 Jiang Duan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamofireObjectMapper.
+FOUNDATION_EXPORT double AlamofireObjectMapperVersionNumber;
+
+//! Project version string for AlamofireObjectMapper.
+FOUNDATION_EXPORT const unsigned char AlamofireObjectMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireObjectMapper/PublicHeader.h>
+
+

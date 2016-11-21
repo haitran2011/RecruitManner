81e60f8805e5ef4388349c4131f552e99779295c
jigang.duan <jigang.duan@tcl.com>
2016-11-21 18:16:42 +0800

    add YZPullDownMenu lib

---
+++ RecruitManner/Pods/Target Support Files/YZPullDownMenu/YZPullDownMenu-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YZPullDownMenu : NSObject
+@end
+@implementation PodsDummy_YZPullDownMenu
+@end
---
+++ RecruitManner/Pods/Target Support Files/YZPullDownMenu/YZPullDownMenu-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "YZCover.h"
+#import "YZPullDownMenu.h"
+
+FOUNDATION_EXPORT double YZPullDownMenuVersionNumber;
+FOUNDATION_EXPORT const unsigned char YZPullDownMenuVersionString[];
+
